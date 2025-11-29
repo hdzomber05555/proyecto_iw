5e9dcdbb223eead486138c3a12bf8b605a1e0015
Mauricio-27 <rodrigoloxx121@gmail.com>
2025-11-29 17:28:49 +0100

    mejora

--- public/register.php
+++ public/register.php
@@ -6,7 +6,9 @@ require_once __DIR__ . '/../app/utils.php';
 require_once __DIR__ . '/../app/csrf.php';
 
 // Si ya está logueado, no tiene sentido registrarse, lo mandamos al panel
-session_start();
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
 if (isset($_SESSION['usuario_id'])) {
     redirect('index.php');
 }
